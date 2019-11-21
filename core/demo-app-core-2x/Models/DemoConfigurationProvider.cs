0a09c7449663aff37fc2683a2719af651175aa6c
Steve Powell <spowell@descartes.com>
2019-11-21 17:01:46 -0500

    Load configuration from service

---
+++ core/demo-app-core-2x/Models/DemoConfigurationProvider.cs
@@ -0,0 +1,47 @@
+﻿using HT.Config.Shared.SettingsServiceModels;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace demo_app_core_2x.Models
+{
+    public class RemoteConfigOptions
+    {
+        public int RefreshIntervalMs { get; set; } = 5 * 60 * 1000;
+        public string RemoteConfigUrl { get; set; } //= ConfigurationManager.ConnectionStrings["HTConfigService:BaseUrl"].ConnectionString;
+
+        public Action<RemoteConfigContext> OnGetContext { get; set; }
+        public Action<Exception> OnError { get; set; }
+    }
+    public class RemoteConfigContext
+    {
+        private static string __machineName = Environment.MachineName;
+
+        public string AppName { get; set; }
+        //=
+        //    ConfigurationManager.AppSettings["ApplicationName"]
+        //    ?? ConfigurationManager.AppSettings["AppName"]
+        //    ?? ConfigurationManager.AppSettings["HTConfigService:ApplicationName"];
+
+        public string EnvironmentName { get; set; } =
+            Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+            ?? Environment.GetEnvironmentVariable("DOTNETCORE_ENVIRONMENT");
+            //?? ConfigurationManager.AppSettings["EnvironmentName"]
+            //    ?? ConfigurationManager.AppSettings["HTConfigService:EnvironmentName"];
+
+        public string Override { get; set; } 
+            //= ConfigurationManager.AppSettings["Override"]
+            //?? ConfigurationManager.AppSettings["HTConfigService:Override"];
+        public string ActorName { get; set; } = "Network Service";
+        public string ActorHostName { get; set; } = __machineName;
+    }
+  
+    
+}
--- core/demo-app-core-2x/Program.cs
+++ core/demo-app-core-2x/Program.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using demo_app_core_2x.Models;
+using HT.Config.Shared.SettingsServiceModels;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -27,56 +30,141 @@ namespace demo_app_core_2x
                 .ConfigureAppConfiguration((hostContext, configBuilder) =>
                 {
                     var settings = configBuilder.Build();
-                    configBuilder.AddHTConfigurationSource(settings["ConnectionStrings:ConfigServiceUrl"]);                    
+                    RemoteConfigOptions remoteOptions = new RemoteConfigOptions();
+                    settings.Bind("HTConfiguration", remoteOptions);
+                    remoteOptions.RemoteConfigUrl = settings["ConnectionStrings:ConfigServiceUrl"];
+
+                    RemoteConfigContext callerContext = new RemoteConfigContext();
+                    settings.Bind("HTConfiguration", callerContext);
+
+                    configBuilder.AddHTConfigurationSource(remoteOptions,callerContext);                    
                 })
                 .UseStartup<Startup>();
     }
 
     public static class HTConfigurationExtensions
     {
-        public static IConfigurationBuilder AddHTConfigurationSource(this IConfigurationBuilder configBuilder, string connectionString)
+        public static IConfigurationBuilder AddHTConfigurationSource(this IConfigurationBuilder configBuilder, RemoteConfigOptions options, RemoteConfigContext context)
         {
-            configBuilder.Sources.Add(new HTConfigurationSource(connectionString));
+            configBuilder.Sources.Add(new HTConfigurationSource(options,context));
             return configBuilder;
         }
     }
     public class HTConfigurationSource:IConfigurationSource
-    {
-        private object connnectionString;
+    {       
+        private RemoteConfigOptions _options;
+        private RemoteConfigContext _context;
 
-        public HTConfigurationSource(string connectionString)
+        
+        public HTConfigurationSource(RemoteConfigOptions options, RemoteConfigContext context)
         {
-            var _cnString = connnectionString;
+            this._options = options;
+            this._context = context;
         }
 
         public IConfigurationProvider Build(IConfigurationBuilder builder)
         {
-            throw new NotImplementedException();
+            return new HTConfigurationProvider(_options, _context);
         }
         
     }
     public class HTConfigurationProvider : ConfigurationProvider
     {
-        private string _cnString;
-
-        public HTConfigurationProvider(string connectionString)
+        
+        private RemoteConfigOptions _options;
+        private RemoteConfigContext _context;
+        private Timer _timer;
+        public HTConfigurationProvider(RemoteConfigOptions options, RemoteConfigContext context)
         {
-            _cnString = connectionString;
-        }
+            this._options = options;
+            this._context = context;
+            _timer = new Timer(onLoadConfigurationTimer, null, Timeout.Infinite, Timeout.Infinite);
+    }
 
         public override void Load()
         {
-            using(var httpClient = new HttpClient())
+            loadConfiguration();
+            //startTimer();
+           
+        }
+        private void onLoadConfigurationTimer(object state)
+        {
+            stopTimer();
+            loadConfiguration();
+            startTimer();
+        }
+        public HTConfigurationProvider Start()
+        {
+            onLoadConfigurationTimer(null);
+            return this;
+        }
+        private void startTimer()
+        {
+           // ConfigurationManager.AppSettings.Set("__htconfig:nextloadtime", DateTimeOffset.Now.AddMilliseconds(_options.RefreshIntervalMs).ToString());
+            _timer.Change(_options.RefreshIntervalMs, _options.RefreshIntervalMs);
+        }
+        private void loadConfiguration()
+        {
+            var ctx = new RemoteConfigContext();
+            try
             {
-                httpClient.BaseAddress = new Uri(_cnString, UriKind.RelativeOrAbsolute);
-                httpClient.DefaultRequestHeaders.Accept.Clear();
-                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                _options?.OnGetContext?.Invoke(ctx);
+                //ConfigurationManager.AppSettings.Set("__htconfig:lastloadtime", DateTimeOffset.Now.ToString());
+                //ConfigurationManager.AppSettings.Set("__htconfig:lastloadContext", JsonConvert.SerializeObject(ctx));
 
-              //  var contentToSend = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri(_options.RemoteConfigUrl, UriKind.RelativeOrAbsolute);
+                    httpClient.DefaultRequestHeaders.Accept.Clear();
+                    httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    SettingsRequest request = new SettingsRequest()
+                    {
+                        ActorHostName = ctx.ActorHostName,
+                        ActorName = ctx.ActorName,
+                        AppName = ctx.AppName,
+                        EnvironmentName = ctx.EnvironmentName,
+                        Override = ctx.Override
+                    };
+                    var contentToSend = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
 
-             //   var httpResult = httpClient.PostAsync("api/settings", contentToSend).ConfigureAwait(false).GetAwaiter().GetResult();
+                    var httpResult = httpClient.PostAsync("api/settings", contentToSend).ConfigureAwait(false).GetAwaiter().GetResult();
+                    if (httpResult.IsSuccessStatusCode)
+                    {
+                        var httpResultString = httpResult.Content.ReadAsStringAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+                        SettingsResponse response = JsonConvert.DeserializeObject<SettingsResponse>(httpResultString);
+                        if (response.IsOk)
+                        {
+                            foreach (var setting in response.Settings)
+                            {
+                                if (Data.ContainsKey(setting.Value.Key))
+                                {
+                                    Data[setting.Value.Key] = setting.Value.Value;
+                                }
+                                else
+                                {
+                                    Data.Add(setting.Value.Key, setting.Value.Value);
+                                }
+                            }
+                            // write results to secondary configuration store
+                        }
+                    }
+                    else
+                    {
+                        // read results from secondary configuration store
+                    }
+                }
+//                ConfigurationManager.AppSettings.Set("__htconfig:lastloadResult", "OK");
             }
-            base.Load();
+            catch (Exception ex)
+            {
+              //  ConfigurationManager.AppSettings.Set("__htconfig:lastloadResult", JsonConvert.SerializeObject(ex));
+                _options.OnError?.Invoke(ex);
+            }
+        }
+
+        private void stopTimer()
+        {
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
         }
     }
 
